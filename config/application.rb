72f82cb6026286ca20c44a4ae09ae5e3a11c2fb0
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-18 09:15:32 -0400

    Complete lab

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module BlogFlash
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.action_controller.permit_all_parameters = true
   end
 end
